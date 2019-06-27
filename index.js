d41178cc6c452464df5f79c4571bb09d816fe7a6
merry-paradigm-9237 <k.merriman32@hotmail.com>
2019-06-27 02:47:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function multiply(a,b) {
 function divide(a,b) {
   return a/b
 }
-function inc(n) {
-return (n + 1 )
+function inc(v) {
+return (v + 1 )
 }
-function inc(n) {
-  return (n - 1)
+function inc(v) {
+  return (v - 1)
 }
